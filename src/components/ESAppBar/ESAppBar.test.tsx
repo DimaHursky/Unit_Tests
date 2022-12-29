eabd67dfbac9d8d2041d6f81b1fc893a8708bf91
DimaHursky <77124180+DimaHursky@users.noreply.github.com>
2022-12-29 12:00:44 +0200

    Update ESAppBar.test.tsx

--- src/components/ESAppBar/ESAppBar.test.tsx
+++ src/components/ESAppBar/ESAppBar.test.tsx
@@ -9,6 +9,39 @@ import { Box } from '@mui/material';
 import NotificationAddIcon from '@mui/icons-material/NotificationAdd';
 import AppsIcon from '@mui/icons-material/Apps';
 
+const MockOfficeSwitcher = () => <Box>Office Switcher</Box>;
+
+const MockSearchField = () => <Box>Search Field</Box>;
+
+const MockNotifications = () => (
+  <Box>
+    <NotificationAddIcon />
+  </Box>
+);
+
+const MockApps = () => (
+  <Box>
+    <AppsIcon />
+  </Box>
+);
+
+const MockProfileButton = () => <Box>Profile</Box>;
+
+const ESAppBarr = () => {
+  return (
+    <EcosystemThemeProvider theme={LightTheme}>
+      <ESAppBar
+        officeSwitcher={<MockOfficeSwitcher />}
+        showNavigationToggle
+        searchField={<MockSearchField />}
+        notifications={<MockNotifications />}
+        apps={<MockApps />}
+        profileButton={<MockProfileButton />}
+      />
+    </EcosystemThemeProvider>
+  );
+};
+
 describe('Running Test for ESAppBar', () => {
   test('Component ESAppBar is rendered', () => {
     const MockOfficeSwitcher = () => <Box>Office Switcher</Box>;
@@ -42,5 +75,24 @@ describe('Running Test for ESAppBar', () => {
       </EcosystemThemeProvider>,
     );
     expect(screen.getByTestId('appbar')).toBeInTheDocument();
+  });
+
+  it('Component ESAppBar is rendered', () => {
+    render(<ESAppBarr />);
+    expect(screen.getByText('Office Switcher')).toBeInTheDocument();
+    expect(screen.getByText('Search Field')).toBeInTheDocument();
+    expect(screen.getByText('Profile')).toBeInTheDocument();
+    // expect(screen.queryByText('Promote')).toBeNull();
+
+    screen.debug();
+    screen.logTestingPlaygroundURL();
+  });
+
+  it('', () => {
+    render(<ESAppBarr />);
+  });
+
+  it('', () => {
+    render(<ESAppBarr />);
   });
 });
